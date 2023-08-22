ca18043b86d5db7a6904497328e552502edbb0b9
DhanushNKuchally-creator <141710816+DhanushNKuchally-creator@users.noreply.github.com>
2023-08-22 11:21:30 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -28,33 +28,40 @@ def count_duplicate_words(words):
     return duplicate_word_count
 
 def main():
-    print("Enter the Statement: ")
-    statement = input()
+    try:
+        print("Enter the Statement: ")
+        statement = input()
+        
+        if len(statement) == 0:
+            print("The input cannot be empty")
+        else:
+            repeated_chars = count_duplicates(statement)
+            total_repeated_chars = len(repeated_chars)
 
-    repeated_chars = count_duplicates(statement)
-    total_repeated_chars = len(repeated_chars)
+            words = statement.split()
+            total_words = len(words)
 
-    words = statement.split()
-    total_words = len(words)
+            duplicate_word_count = count_duplicate_words(words)
 
-    duplicate_word_count = count_duplicate_words(words)
+            reversed_chars = statement[::-1]
+            reversed_words = ' '.join(reversed(words))
 
-    reversed_chars = statement[::-1]
-    reversed_words = ' '.join(reversed(words))
+            new_statement = reversed_words
+            simplified_new_statement = ""
+            for char in new_statement:
+                if char not in simplified_new_statement:
+                    simplified_new_statement += char
 
-    new_statement = reversed_words
-    simplified_new_statement = ""
-    for char in new_statement:
-        if char not in simplified_new_statement:
-            simplified_new_statement += char
+            print(f"Total number of characters: {len(statement)}")
+            print(f"Total number of duplicate characters: {total_repeated_chars}")
+            print(f"Total number of words: {total_words}")  
+            print(f"Total number of duplicate words: {duplicate_word_count}")
+            print(f"Reversed characters: {reversed_chars}")
+            print(f"Reversed words: {reversed_words}")
+            print(f"New statement from reversed words: {simplified_new_statement}")
 
-    print(f"Total number of characters: {len(statement)}")
-    print(f"Total number of duplicate characters: {total_repeated_chars}")
-    print(f"Total number of words: {total_words}")
-    print(f"Total number of duplicate words: {duplicate_word_count}")
-    print(f"Reversed characters: {reversed_chars}")
-    print(f"Reversed words: {reversed_words}")
-    print(f"New statement from reversed words: {simplified_new_statement}")
+    except Exception as e:
+        print("An error occurred:", e)
 
 if __name__ == "__main__":
     main()
