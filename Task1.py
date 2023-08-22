165d834b3ff06d8432047d693971733a5d6516ac
DhanushNKuchally-creator <141710816+DhanushNKuchally-creator@users.noreply.github.com>
2023-08-22 10:51:14 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -1,4 +1,3 @@
-
 def count_duplicates(statement):
     char_count = {}
     repeated_chars = []
@@ -14,6 +13,20 @@ def count_duplicates(statement):
 
     return repeated_chars
 
+def count_duplicate_words(words):
+    word_count = {}
+    duplicate_word_count = 0
+
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+            if word_count[word] == 2:
+                duplicate_word_count += 1
+        else:
+            word_count[word] = 1
+
+    return duplicate_word_count
+
 def main():
     print("Enter the Statement: ")
     statement = input()
@@ -21,37 +34,27 @@ def main():
     repeated_chars = count_duplicates(statement)
     total_repeated_chars = len(repeated_chars)
 
-    # Total number of characters
-    total_characters = len(statement)
-
-    # Total number of words
     words = statement.split()
     total_words = len(words)
 
-    # Reverse characters and words
+    duplicate_word_count = count_duplicate_words(words)
+
     reversed_chars = statement[::-1]
     reversed_words = ' '.join(reversed(words))
 
-    # Form a new statement from reversed words
     new_statement = reversed_words
-
-    # Remove duplicate characters from the new statement
     simplified_new_statement = ""
     for char in new_statement:
         if char not in simplified_new_statement:
             simplified_new_statement += char
 
-    # Print results
-    print(f"Total number of characters: {total_characters}")
+    print(f"Total number of characters: {len(statement)}")
     print(f"Total number of duplicate characters: {total_repeated_chars}")
     print(f"Total number of words: {total_words}")
+    print(f"Total number of duplicate words: {duplicate_word_count}")
     print(f"Reversed characters: {reversed_chars}")
     print(f"Reversed words: {reversed_words}")
     print(f"New statement from reversed words: {simplified_new_statement}")
 
 if __name__ == "__main__":
     main()
-
-
-
-
